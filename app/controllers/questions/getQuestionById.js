88af438da0dd69c9989ccdd10c4c8bc2ad7e38b2
Aldo Dominguez <aldo.dominguez@wizeline.com>
2022-10-06 22:11:00 -0500

    add get question by id controller

--- app/controllers/questions/getQuestionById.js
+++ app/controllers/questions/getQuestionById.js
@@ -4,7 +4,7 @@ import {
   QUESTION_NOT_FOUND_ERROR_MESSAGE
 } from "~/utils/constants";
 
-export const getQuestionById = async (questionId) => {
+export const getQuestionById = async (questionId, user) => {
   if (!questionId || typeof questionId !== "number" || parseInt(questionId) < 1) {
     return {
       error: {
@@ -13,33 +13,61 @@ export const getQuestionById = async (questionId) => {
       },
     };
   }
+
+  if (!user) {
+    return {
+      error: {
+        message: INVALID_PARAMS_FOR_OPERATION_ERROR_MESSAGE,
+        detail: "Please provide the user object",
+      },
+    };
+  }
   
   try {
-    const question = await db.Questions.findUnique({
+    const unmappedQuestion = await db.Questions.findUnique({
         where: { question_id : questionId },
         include: {
-          created_by: true,
-          assigned_to: true,
+          _count: {
+            select: {
+              Comments: true,
+              Votes: true,
+            },
+          },
+          Votes: true,
           Answers: {
             include: {
               Nps: true,
-              user: true,
+              AnsweredBy: true,
             }
           },
-          Department: {
-            select: {
-              name: true,
-            }
-          }
+          created_by: true,
+          Department: true,
         }
       });
+      const hasUserData = user && user.id;
+      const hasAnswer = unmappedQuestion.Answers.length > 0;
+
+      const Answer = unmappedQuestion.Answers.length < 1 
+        ? null
+        : {
+          ...unmappedQuestion.Answers[0],
+          hasScored: (hasUserData && hasAnswer && unmappedQuestion.Answers[0].Nps.some((nps) => nps.user === user.id)) ?? false,
+        }; 
+
+      const mappedQuestion = {
+        ...unmappedQuestion,
+        hasVoted: unmappedQuestion.Votes.some((vote) => vote.user === user.id) ?? false,
+        num_votes: unmappedQuestion._count.Votes,
+        numComments: unmappedQuestion._count.Comments,
+        Answer,
+      }
+
       return {
         success: true,
-        question,
+        question: mappedQuestion,
       }
+
   } catch (error) {
-    console.log('Actual error');
-    console.log(error);
     return {
         error: {
             message: QUESTION_NOT_FOUND_ERROR_MESSAGE,
--- tests/integration/controllers/questions/getQuestionById.test.js
+++ tests/integration/controllers/questions/getQuestionById.test.js
@@ -3,15 +3,60 @@ import { db } from "~/utils/db.server";
 
 describe("questions controller", () => {
   describe("get question by id", () => {
-    it("returns the question with all aggregated properties", async () => {
-      const question = (await getQuestionById(2)).question;
-      console.log('The question by id is');
-      console.log(question);
-      console.log('The question department is');
-      console.log(question.Department);
-      console.log('Answer');
-      console.log(question.Answers[0]);
-      expect(true).toBe(false);
+    const dbFindUniqueSpy = jest.spyOn(db.Questions, "findUnique");
+
+    it('returns an error when provided invalid id', async () => {
+      const response = await getQuestionById('test', { id: 'google-oauth2|108653070533260305238' });
+      expect(response).toBeDefined();
+      expect(response.error).toBeDefined();
+      expect(response.error.message).toBe('The provided parameters for the operation are not valid');
+      expect(response.error.detail).toBe('The question id must be an integer not minor to 1');
+      expect(dbFindUniqueSpy).toHaveBeenCalledTimes(0);
     });
+
+    it('returns an error when not provided user object', async () => {
+      const response = await getQuestionById(1);
+      expect(response).toBeDefined();
+      expect(response.error).toBeDefined();
+      expect(response.error.message).toBe('The provided parameters for the operation are not valid');
+      expect(response.error.detail).toBe('Please provide the user object');
+      expect(dbFindUniqueSpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('returns an error when the question is not found', async () => {
+      const response = await getQuestionById(1001, { id: 'google-oauth2|108653070533260305238' });
+      expect(response).toBeDefined();
+      expect(response.error).toBeDefined();
+      expect(response.error.message).toBe('The question with the id provided could not be found');
+      expect(response.error.detail).toBe('The question with the id provided could not be found');
+      expect(dbFindUniqueSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it("returns the question with all aggregated properties", async () => {
+      const response = await getQuestionById(2, { id: 'google-oauth2|108653070533260305238' });
+      expect(response).toBeDefined();
+      expect(response.success).toBeDefined();
+      expect(response.success).toBe(true);
+      expect(response.question).toBeDefined();
+
+      const question = response.question;
+      expect(question.question_id).toBe(2);
+      expect(question.question).toContain('Testing');
+      expect(question.is_anonymous).toBe(false);
+      expect(question.location).toBe('ALL');
+      expect(question.num_votes).toBe(1);
+      expect(question.hasVoted).toBe(true);
+      expect(question.created_by).toBeDefined();
+      expect(question.created_by.employee_id).toBe(1);
+      expect(question.created_by.full_name).toBe('Patrick Shu');
+      expect(question.Answer).toBeDefined();
+      expect(question.Answer.answer_id).toBe(1);
+      expect(question.Answer.answer_text).toContain('Testing admin answer');
+      expect(question.Answer.hasScored).toBe(true);
+      expect(question.Department).toBeDefined();
+      expect(question.Department.name).toBe('CEO / Exec Staff');
+      expect(dbFindUniqueSpy).toHaveBeenCalledTimes(2);
+    });
+    
   });
 });

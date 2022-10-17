deb9202f696a18cd44b88b589c87e2e082660145
Luis Alonso Martínez García <71342528+AlonsoMtzG@users.noreply.github.com>
2022-10-17 17:26:30 -0500

    feat(migration): add answers controllers (#34)

---
+++ app/controllers/answers/create.js
@@ -0,0 +1,43 @@
+import moment from 'moment';
+import {
+  DATE_TIME_FORMAT,
+  DEFAULT_ERROR_MESSAGE,
+} from '~/utils/backend/constants';
+import { createAnswerSchema } from '~/utils/backend/validators/answer';
+import { sanitizeHTML } from '~/utils/backend/sanitizer';
+import { db } from '~/utils/db.server';
+
+export const createAnswer = async (body) => {
+  const { error, value } = createAnswerSchema.validate(body);
+
+  if (error) {
+    return {
+      error: { message: DEFAULT_ERROR_MESSAGE, detail: error.message },
+    };
+  }
+
+  const { answered_by_employee_id, answered_question_id, ...rest } = value;
+
+  let answer = await db.Answers.create({
+    data: {
+      ...rest,
+      answer_text: sanitizeHTML(value.answer_text),
+      answer_date: moment.utc().format(DATE_TIME_FORMAT),
+      AnsweredBy: {
+        connect: {
+          employee_id: answered_by_employee_id,
+        },
+      },
+      Question: {
+        connect: {
+          question_id: answered_question_id,
+        },
+      },
+    },
+  });
+
+  return {
+    success: 'Answer has been created succesfully!',
+    answer,
+  };
+};
---
+++ app/utils/backend/validators/answer.js
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+import { MAXIMUM_ANSWER_LENGTH, MINIMUM_ANSWER_LENGTH } from '../constants';
+import { JOI_ID_VALIDATION } from '../joiConstants';
+
+export const createAnswerSchema = Joi.object().keys({
+  answer_text: Joi.string()
+    .min(MINIMUM_ANSWER_LENGTH)
+    .max(MAXIMUM_ANSWER_LENGTH)
+    .required(),
+  answered_by_employee_id: JOI_ID_VALIDATION,
+  answered_question_id: JOI_ID_VALIDATION,
+});
+
+export const updateAnswerSchema = Joi.object().keys({
+  answer_id: JOI_ID_VALIDATION,
+  answer_text: Joi.string()
+    .min(MINIMUM_ANSWER_LENGTH)
+    .max(MAXIMUM_ANSWER_LENGTH)
+    .required(),
+});
+
+export const deleteAnswerSchema = Joi.object().keys({
+  answer_id: JOI_ID_VALIDATION,
+});

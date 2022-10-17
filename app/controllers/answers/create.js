43922f51bda98cb5b5b981a3f63ef1a0593fd31c
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2022-10-17 14:44:16 -0500

    createAnswer update

--- app/controllers/answers/create.js
+++ app/controllers/answers/create.js
@@ -1,4 +1,8 @@
-import { DEFAULT_ERROR_MESSAGE } from '~/utils/backend/constants';
+import moment from 'moment';
+import {
+  DATE_TIME_FORMAT,
+  DEFAULT_ERROR_MESSAGE,
+} from '~/utils/backend/constants';
 import { createAnswerSchema } from '~/utils/backend/validators/answer';
 import { sanitizeHTML } from '~/utils/backend/sanitizer';
 import { db } from '~/utils/db.server';
@@ -14,13 +18,11 @@ export const createAnswer = async (body) => {
 
   const { answered_by_employee_id, answered_question_id, ...rest } = value;
 
-  const date = new Date('October 17, 2022 03:24:00');
-
-  let created = await db.Answers.create({
+  let answer = await db.Answers.create({
     data: {
       ...rest,
       answer_text: sanitizeHTML(value.answer_text),
-      answer_date: date,
+      answer_date: moment.utc().format(DATE_TIME_FORMAT),
       AnsweredBy: {
         connect: {
           employee_id: answered_by_employee_id,
@@ -36,6 +38,6 @@ export const createAnswer = async (body) => {
 
   return {
     success: 'Answer has been created succesfully!',
-    answer: created,
+    answer,
   };
 };

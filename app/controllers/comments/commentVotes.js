5ab67456b51c19e851428bdb3f47b44b22ff5573
AlonsoMtzG <71342528+AlonsoMtzG@users.noreply.github.com>
2022-10-21 14:01:06 -0500

    commentVote controller

---
+++ app/controllers/comments/commentVotes.js
@@ -0,0 +1,88 @@
+import { DEFAULT_ERROR_MESSAGE } from '~/utils/backend/constants';
+import {
+  commentVoteExistsSchema,
+  commentVoteSchema,
+} from '~/utils/backend/validators/comment';
+import { db } from '~/utils/db.server';
+
+const transformValue = (value) => {
+  let transformedValue = 0;
+
+  if (value >= 1) {
+    transformedValue = 1;
+  } else if (value <= -1) {
+    transformedValue = -1;
+  }
+
+  return transformedValue;
+};
+
+const findCommentVote = async (query) => {
+  const { error, value } = commentVoteExistsSchema.validate(query);
+  const { comment_id, user } = value;
+
+  if (error) {
+    return {
+      error: { message: DEFAULT_ERROR_MESSAGE, detail: error.details },
+    };
+  }
+
+  const commentVote = await db.CommentVote.findFirst({
+    where: {
+      comment_id,
+      user,
+    },
+  });
+
+  return commentVote;
+};
+
+export const upsertCommentVote = async (query) => {
+  const { error, value } = commentVoteSchema.validate(query);
+  const { comment_id, user, value: newValue } = value;
+
+  if (error) {
+    return {
+      error: { message: DEFAULT_ERROR_MESSAGE, detail: error.details },
+    };
+  }
+
+  const foundComment = await findCommentVote({ comment_id, user });
+
+  if (foundComment) {
+    const alreadyUpvoted = foundComment.value >= 1 && newValue >= 1;
+    const alreadyDownvoted = foundComment.value <= -1 && newValue <= -1;
+
+    let temp;
+
+    if (alreadyUpvoted || alreadyDownvoted) {
+      temp = 0;
+    } else {
+      temp = newValue;
+    }
+
+    const commentVote = await db.CommentVote.update({
+      where: {
+        id: foundComment.id,
+      },
+      data: {
+        value: transformValue(temp),
+      },
+    });
+
+    return {
+      success: 'Vote updated successfully!',
+      commentVote,
+    };
+  }
+
+  const commentVote = await db.CommentVote.create({
+    data: {
+      user,
+      comment_id,
+      value: transformValue(newValue),
+    },
+  });
+
+  return { success: 'Vote created successfully!', commentVote };
+};
---
+++ tests/integration/controllers/comments/commentVotes.test.js
@@ -0,0 +1,78 @@
+import { upsertCommentVote } from '~/controllers/comments/commentVotes';
+import { db } from '~/utils/db.server';
+
+describe('commentVotes', () => {
+  const dbFindFirstCommentVoteSpy = jest.spyOn(db.CommentVote, "findFirst");
+  const dbUpdateCommentVoteSpy = jest.spyOn(db.CommentVote, "update");
+  const dbCreateCommentVoteSpy = jest.spyOn(db.CommentVote, "create");
+  
+  it('creates vote', async () => {
+    const payload = {
+      comment_id: 1,
+      user: 'google-oauth2|100361217454991890939',
+      value: -1,
+    };
+    const response = await upsertCommentVote(payload);
+    expect(response).toBeDefined();
+    expect(response.commentVote.value).toBe(-1);
+    expect(dbFindFirstCommentVoteSpy).toHaveBeenCalledTimes(1);
+    expect(dbUpdateCommentVoteSpy).toHaveBeenCalledTimes(0);
+    expect(dbCreateCommentVoteSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('sends same current value (Up)', async () => {
+    const payload = {
+      comment_id: 10,
+      user: 'google-oauth2|114355703508299539934',
+      value: 1,
+    };
+    const response = await upsertCommentVote(payload);
+    expect(response).toBeDefined();
+    expect(response.commentVote.value).toBe(0);
+    expect(dbFindFirstCommentVoteSpy).toHaveBeenCalledTimes(2);
+    expect(dbUpdateCommentVoteSpy).toHaveBeenCalledTimes(1);
+    expect(dbCreateCommentVoteSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('sends same current value (Down)', async () => {
+    const payload = {
+      comment_id: 2,
+      user: 'google-oauth2|100361217454991890939',
+      value: -1,
+    };
+    const response = await upsertCommentVote(payload);
+    expect(response).toBeDefined();
+    expect(response.commentVote.value).toBe(0);
+    expect(dbFindFirstCommentVoteSpy).toHaveBeenCalledTimes(3);
+    expect(dbUpdateCommentVoteSpy).toHaveBeenCalledTimes(2);
+    expect(dbCreateCommentVoteSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('changes value from Up to Down', async () => {
+    const payload = {
+      comment_id: 10,
+      user: 'google-oauth2|114355703508299539934',
+      value: -1,
+    };
+    const response = await upsertCommentVote(payload);
+    expect(response).toBeDefined();
+    expect(response.commentVote.value).toBe(-1);
+    expect(dbFindFirstCommentVoteSpy).toHaveBeenCalledTimes(4);
+    expect(dbUpdateCommentVoteSpy).toHaveBeenCalledTimes(3);
+    expect(dbCreateCommentVoteSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('changes value from Down to Up', async () => {
+    const payload = {
+      comment_id: 2,
+      user: 'google-oauth2|100361217454991890939',
+      value: 1,
+    };
+    const response = await upsertCommentVote(payload);
+    expect(response).toBeDefined();
+    expect(response.commentVote.value).toBe(1);
+    expect(dbFindFirstCommentVoteSpy).toHaveBeenCalledTimes(5);
+    expect(dbUpdateCommentVoteSpy).toHaveBeenCalledTimes(4);
+    expect(dbCreateCommentVoteSpy).toHaveBeenCalledTimes(1);
+  });
+});

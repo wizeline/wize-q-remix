f1a3bc17eb9e4c962e0a2aaafe2fcb0ab15bfd2f
Aldo Dominguez <104774638+AldoDominguezWizeline@users.noreply.github.com>
2022-10-21 10:18:41 -0500

    feat(migration): Add update comment controller with integration tests (#49)

--- app/utils/backend/validators/comments.js
+++ app/utils/backend/validators/comments.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { JOI_ID_VALIDATION, JOI_SIMPLE_STRING_VALIDATION } from '~/utils/backend/joiConstants';
+import { MINIMUM_COMMENT_LENGTH, MAXIMUM_COMMENT_LENGTH } from '~/utils/backend/constants';
 
 const EMAIL_VALIDATION = Joi.string().email().allow(null);
 const SIMPLE_INTEGER_VALIDATION = Joi.number().integer();
@@ -12,5 +13,12 @@ export const getCommentsSchema = Joi.object().keys({
   sessionToken: JOI_SIMPLE_STRING_VALIDATION,
   userId: JOI_SIMPLE_STRING_VALIDATION,
   sortBy: JOI_SIMPLE_STRING_VALIDATION,
+});
+
+export const updateCommentSchema = Joi.object().keys({
+  commentId: JOI_ID_VALIDATION,
+  comment: Joi.string().min(MINIMUM_COMMENT_LENGTH).max(MAXIMUM_COMMENT_LENGTH).required(),
+  accessToken: Joi.string().required(),
+  userEmail: EMAIL_VALIDATION,
 });
 

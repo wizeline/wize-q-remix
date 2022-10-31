b2d43ad9a9056d3782eb679ed544f0658973a880
Alberto Castañeda <alberto.castaneda@wizeline.com>
2022-10-31 09:42:27 -0700

    fix: add typescript to project

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {
-        async startDatabase({}) {
+        async startDatabase() {
           console.log("Initiating test database setup...");
           await initTestDb();
           return null
         },
-        async destroyDatabase({}) {
+        async destroyDatabase() {
           console.log("Destroying test database...");
           await destroyDb();
           return null

bcd3f10fb50060b040a3eda496f8f13b22c81486
Alberto Castañeda Arana <alberto.castaneda@wizeline.com>
2022-11-01 15:12:49 -0700

    feat: add cypress configuration and initial tests (#64)

---
+++ tests/cypress/e2e/home.cy.js
@@ -0,0 +1,60 @@
+describe('Home', () => {
+  before(() => {
+    cy.task("destroyDatabase", {})
+    cy.task("startDatabase", {})
+  })
+
+  after(() => {
+    cy.task("destroyDatabase", {})
+  })
+
+  it('redirect to login if not authenticated ', () => {
+    cy.visit('http://localhost:3000');
+    cy.url().should('be.equal', 'http://localhost:3000/login')
+  })
+
+  it('remain at home if authenticated', () => {
+    cy.login();
+    cy.visit('http://localhost:3000');
+    cy.url().should('be.equal', 'http://localhost:3000/')
+  });
+
+  it('lists questions', () => {
+    cy.login();
+    cy.visit('http://localhost:3000');
+
+    cy.get("#questions-list");
+    cy.get("#questions-list").children().should('have.length', 10);
+  });
+
+  it('has ask question button', () => {
+    cy.login();
+    cy.visit('http://localhost:3000');
+
+    cy.get("#ask-button").click();
+
+    cy.url().should('be.equal', 'http://localhost:3000/questions/new')
+
+  });
+
+  it('has go to top button', () => {
+    cy.login();
+    cy.visit('http://localhost:3000');
+
+    cy.scrollTo('bottom')
+
+    cy.get("#go-to-top-button").click();
+
+    cy.window().its('scrollY').should('equal', 0);
+
+  });
+
+  it('question click redirects to question detail page', () => {
+    cy.login();
+    cy.visit('http://localhost:3000');
+
+    cy.get("#comments-button-1").click();
+
+    cy.url().should('be.equal', 'http://localhost:3000/questions/1')
+  });
+})

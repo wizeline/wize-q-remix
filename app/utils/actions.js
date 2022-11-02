8460ced00445887881a37300d9ef348f4abc949a
Georgina Fraire Miranda <97203617+GeorginaFraire@users.noreply.github.com>
2022-11-02 14:56:18 -0600

    feat: (migration)Add controller to approve a comment as an answer (#66)

--- app/utils/actions.js
+++ app/utils/actions.js
@@ -4,4 +4,5 @@ export const ACTIONS = {
   CREATE_QUESTION_ANSWER: 'create_question_answer',
   UPDATE_QUESTION_ANSWER: 'edit_question_answer',
   DELETE_ANSWER: 'delete_answer',
+  APPROVED_COMMENT: 'approved_comment'
 };

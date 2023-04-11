75f1da00a98f6768c6682a51f5b9d8678fe85e9d
GeorginaFraire <georgina.fraire@wizeline.com>
2023-04-11 13:20:31 -0600

    Revert "feat: Added public filter to question list (#168)"
    This reverts commit e0dc47764318c444281ef98aea8fcda9df470f79.

--- app/controllers/questions/list.js
+++ app/controllers/questions/list.js
@@ -139,13 +139,12 @@ const buildWhereLastXMonths = (numMonths, dateRange, search) => {
   }
   return {};
 };
-
 const buildWhereIsAdminSearch = (isAdmin) => {
   if (isAdmin) {
     return {};
   }
 
-  return { is_enabled: true, is_public: true };
+  return { is_enabled: true };
 };
 
 const buildWhere = ({

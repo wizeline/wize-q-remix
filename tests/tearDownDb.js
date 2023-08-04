378b68027c0d6dfd3e0ade2acbb952fbda0c4294
Alberto Castañeda Arana <alberto.castaneda@wizeline.com>
2023-08-04 13:16:19 -0600

    chore(CI): github actions postgres (#259)

--- tests/tearDownDb.js
+++ tests/tearDownDb.js
@@ -5,13 +5,28 @@ const tearDownDb = async () => {
 
   // Delete in reverse order
   await db.nps.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "nps_id_seq" RESTART WITH 1;`;
+
   await db.votes.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "votes_id_seq" RESTART WITH 1;`;
+
   await db.answers.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "answers_answer_id_seq" RESTART WITH 1;`;
+
   await db.comments.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "comments_id_seq" RESTART WITH 1;`;
+
   await db.questions.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "questions_question_id_seq" RESTART WITH 1;`;
+
   await db.locations.deleteMany();
+  // await db.$queryRaw`ALTER SEQUENCE "locations_id_seq" RESTART WITH 1;`;
+
   await db.departments.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "departments_department_id_seq" RESTART WITH 1;`;
+
   await db.users.deleteMany();
+  await db.$queryRaw`ALTER SEQUENCE "users_employee_id_seq" RESTART WITH 1;`;
 
   await db.$disconnect();
 };

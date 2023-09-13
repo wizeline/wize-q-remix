85db9e4c3a7500c11090264b061fa7563e90526d
Alberto Castañeda <alberto.castaneda@boats.com>
2023-09-13 15:41:58 -0600

    update find similar function migration to include answer date

--- prisma/migrations/20230912213748_find_similar_function/migration.sql
+++ prisma/migrations/20230912213748_find_similar_function/migration.sql
@@ -1,5 +1,5 @@
 create or replace function find_answer(embedding vector(1536), match_threshold float, match_count int, min_content_length int)
- RETURNS TABLE(answer text, similarity float)
+ RETURNS TABLE(answer text, answer_date date, similarity float)
  LANGUAGE plpgsql
 AS $$
 #variable_conflict use_variable
@@ -7,26 +7,29 @@ begin
   return query
   select
     COALESCE(a.answer, c.comment) AS answer,
+    COALESCE(a.answer_date, c.answer_date) as answer_date,
     (e.embedding <#> embedding) * -1 as similarity
-  from questions q
-  join embeddings e
+    from questions q
+    join embeddings e
     on q.question_id = e.question_id
-  LEFT JOIN (
+    LEFT JOIN (
       SELECT
           answered_question_id,
+          MAX(answer_date) as answer_date,
           MAX(answer_text) AS answer
       FROM wizeq.ANSWERS
       GROUP BY answered_question_id
-  ) a ON q.question_id = a.answered_question_id
-  LEFT JOIN (
+    ) a ON q.question_id = a.answered_question_id
+    LEFT JOIN (
       SELECT
           questionid,
+          MAX(createdat) as answer_date,
           MAX(comment) AS comment
       FROM wizeq.Comments
       WHERE approvedby IS NOT NULL
       GROUP BY questionid
-  ) c ON q.question_id = c.questionid
-  where length(q.question) >= min_content_length
+    ) c ON q.question_id = c.questionid
+     where length(q.question) >= min_content_length
 
   and (e.embedding <#> embedding) * -1 > match_threshold
 

ec011ec185c54d7c9e24505427421dad37a2c6af
Georgina Fraire Miranda <97203617+GeorginaFraire@users.noreply.github.com>
2024-01-11 10:27:48 -0600

    feat: update about page (#297)

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { json } from '@remix-run/node';
 import * as Styled from 'app/styles/About.Styled';
-import meetingIcon from 'app/images/ic_meeting.svg';
-import savingFundIcon from 'app/images/ic_saving_fund.svg';
-import dialogueIcon from 'app/images/ic_dialogue.svg';
-import anonymousIcon from 'app/images/ic_anonymous.svg';
 import { requireAuth } from 'app/session.server';
 
 export const loader = async ({ request }) => {
@@ -21,110 +17,139 @@ function About() {
         <p>
           <strong>Wizeline Questions (WizeQ)</strong>
           {' '}
-          is a communication space where you
-          can express your ideas, learn more about Wizeline, and ask all kinds of
-          questions.
+          is an open forum & platform for knowledge-sharing where
+          you can express your ideas and learn more about Wizeline.
+          We value your contributions and are looking forward to
+          learning from your questions, ideas and suggestions.
         </p>
-        <p>
-          Wizeline Questions is a knowledge base where you can ask for information
-          to a specific department and allow other Wizeliners to benefit from it. For
-          example:
-        </p>
-
-        <Styled.BoxWrapper>
-          <Styled.BoxContainer>
-            <Styled.BoxImageContainer>
-              <Styled.BoxImage src={meetingIcon} />
-              <Styled.BoxBoldText>
-                Are you still having problems with Zoom for your next meeting?
-              </Styled.BoxBoldText>
-            </Styled.BoxImageContainer>
-            <Styled.BoxMetadata>
-              Check Wizeline Questions, someone might have already asked IT the same
-              question. If that is not the case, this is your chance to shine!
-            </Styled.BoxMetadata>
-          </Styled.BoxContainer>
-          <Styled.BoxContainer>
-            <Styled.BoxImageContainer>
-              <Styled.BoxImage src={savingFundIcon} />
-              <Styled.BoxBoldText>
-                Do you want to know more about the savings fund?
-              </Styled.BoxBoldText>
-            </Styled.BoxImageContainer>
-            <Styled.BoxMetadata>
-              Tag People Ops in your question, other Wizeliners will be glad to
-              learn about that same topic, for sure!
-            </Styled.BoxMetadata>
-          </Styled.BoxContainer>
-        </Styled.BoxWrapper>
-        <Styled.AboutMetadata>
-          <p>
-            Information can be easily lost in the flood of comments and pinged posts on
-            Slack. Do you need information to persist?
-            {' '}
-            <strong>
-              Use Wizeline
-              Questions instead!
-            </strong>
-          </p>
-        </Styled.AboutMetadata>
-        <h3>What else?</h3>
-        <Styled.AboutWhatElseContainer>
-          <Styled.AboutWhatElseImg src={dialogueIcon} />
-          <Styled.AboutWHatElseText>
-            <Styled.AboutWHatElseBold>
-              Start a conversation. Is there a topic you want to discuss with
-              other Wizeliners?
-            </Styled.AboutWHatElseBold>
-            <Styled.AboutWhatElseMetadata>
-              This is the place!. You can reply to a question and
-              also hold interesting conversations with other Wizeliners.  Use it as a
-              discussion forum, suggestion what-else, or simply visit to say hello! Help us
-              to keep Wizeline an amazing place!
-            </Styled.AboutWhatElseMetadata>
-          </Styled.AboutWHatElseText>
-        </Styled.AboutWhatElseContainer>
-        <Styled.AboutWhatElseContainer>
-          <Styled.AboutWhatElseImg src={anonymousIcon} />
-          <Styled.AboutWHatElseText>
-            <Styled.AboutWHatElseBold>
-              Is there a sensitive question you want to ask anonymously?
-            </Styled.AboutWHatElseBold>
-            <Styled.AboutWhatElseMetadata>
-              Ask on Wizeline Questions! To promote ownership and open communication,
-              it displays your user name by default when you post a new question.
-              But you can always opt for anonymity.
-            </Styled.AboutWhatElseMetadata>
-          </Styled.AboutWHatElseText>
-        </Styled.AboutWhatElseContainer>
-
-        <h3>Things to Keep in Mind When Asking a Question</h3>
+        <p> Here are some examples of what you can do in the tool: </p>
         <span>
-          <p>
-            We value your ideas, questions, suggestions, and comments. Therefore, we
-            encourage you to use this communication space. Please, when writing a new
-            question or a comment follow these recommendations:
-          </p>
-          <div style={{ textAlign: 'justify' }}>
+          <Styled.ListContainer>
             <ul>
               <li>
-                Strive for constructive open communication. Avoid vagueness.
+                If you have a question, you can check if someone else has asked it before.
               </li>
               <li>
-                Do not demean or degrade others because of their gender, race, age, religion,
-                etc.
+                {' '}
+                If the question wasn’t asked before, it’s your time to shine!
+                Post it and it will be answered shortly.
+                You can tag your question per location and topic,
+                this will make it easier for the corresponding area to respond!
               </li>
               <li>
-                Avoid posting questions that include sexually explicit comments, hate speech,
-                prejudicial remarks, and profanity.
+                Want to get other Wizeliners’ opinions on a specific topic or
+                do you have an idea that you want to share?
+                Start a conversation or participate in any of the previous questions or comments.
               </li>
               <li>
-                Do not mock other members, their comments, profiles, threads, or experiences.
-                Remember, what is funny for you may be offensive to others.
+                Do you want to ask a sensitive question? Post your question anonymously,
+                it will be posted privately and will only be visible to the leader of the
+                area that the question is being directed to, and the platform’s moderators,
+                who will be able to respond directly
               </li>
             </ul>
-          </div>
+          </Styled.ListContainer>
         </span>
+
+        <h3>Things to Keep in Mind When Asking a Question</h3>
+        <p>
+          We value your ideas, questions, suggestions, and comments. Therefore, we
+          encourage you to use this communication space. Please, when writing a new
+          question or a comment follow these recommendations:
+        </p>
+        <Styled.ListContainer>
+          <ul>
+            <li>
+              <strong>Strive for constructive open communication.</strong>
+              <ul>
+                <li>
+                  {' '}
+                  Encourage open dialogue that is constructive
+                  and beneficial to the entire community.
+
+                </li>
+                <li>
+                  Express ideas and opinions respectfully,
+                  fostering an environment conducive to professional growth
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Avoid Vagueness.</strong>
+              <ul>
+                <li>
+                  Clearly articulate questions and responses
+                  to ensure clarity and effective communication
+                </li>
+                <li>
+                  Provide adequate context to promote a better
+                  understanding of the topics being discussed.
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Respect and Inclusion</strong>
+              <ul>
+                <li>
+                  Do not demean or degrade others based on gender,
+                  race, age, religion, or any other personal attributes.
+                </li>
+                <li>
+                  Foster an inclusive environment that welcomes
+                  diverse perspectives and experiences.
+                </li>
+              </ul>
+            </li>
+            <li>
+              {' '}
+              <strong> Mindful Language Use:</strong>
+              {' '}
+            </li>
+            <ul>
+              <li>
+                {' '}
+                Avoid posting questions or comments that include sexually explicit
+                content, hate speech, prejudicial remarks, or profanity.
+
+              </li>
+              <li> Choose language that promotes a positive and respectful atmosphere.</li>
+            </ul>
+
+            <li>
+              <strong> Avoid Mockery and Offense:</strong>
+              {' '}
+            </li>
+            <ul>
+              <li> Refrain from mocking other members, their comments, or experiences.</li>
+              <li>
+                {' '}
+                Be mindful that humor is subjective, and what may be funny
+                to one person could be offensive to another.
+                {' '}
+              </li>
+            </ul>
+            <li>
+              {' '}
+              <strong> Confidentiality and Privacy:</strong>
+              {' '}
+            </li>
+            <ul>
+              <li>
+                {' '}
+                Do not post sensitive or confidential information about
+                individuals, projects, or about Wizeline.
+
+              </li>
+              <li>
+                {' '}
+                Protect the privacy of fellow Wizeliners and uphold
+                the trust within our community.
+              </li>
+            </ul>
+
+          </ul>
+
+        </Styled.ListContainer>
       </Styled.AboutBody>
       <Styled.AboutFooter>
         <p>
@@ -132,10 +157,10 @@ function About() {
           {' '}
           Wizeline Questions is an internal project and
           everyone can contribute. Come aboard and meet the team at
-          <a href="https://wizeline.slack.com/archives/C031D9DP7C2" target="_blank" rel="noopener noreferrer">#wize-q-2022</a>
+          <a href="https://wizeline.slack.com/archives/C031D9DP7C2" target="_blank" rel="noopener noreferrer"> #wize-q-2022</a>
           . You can take a
           look at our
-          <a href="https://github.com/wizeline/wize-q-remix">source code</a>
+          <a href="https://github.com/wizeline/wize-q-remix"> source code</a>
           {' '}
           on GitHub.
         </p>

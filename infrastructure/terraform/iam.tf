4f31258a71661c376bed68068cca738376641972
Alberto Castañeda <alberto.castaneda@gmail.com>
2024-03-11 15:44:00 -0600

    Revert "feat(infra): Add SA for WizePrompt team with read permissions on CloudSQL instances (#307)"
    This reverts commit b3ad8ee6052f106ea3053cbd8827a210adf9d67e.

--- infrastructure/terraform/iam.tf
+++
@@ -1,7 +0,0 @@
-# CloudSQL Read Permissions for WizePrompt SA
-resource "google_project_iam_member" "wizeprompt__cloudsql__reader" {
-  role = "roles/cloudsql.viewer"
-  members = [
-    "serviceAccount:${google_service_account.wizeprompt.email}",
-  ]
-}

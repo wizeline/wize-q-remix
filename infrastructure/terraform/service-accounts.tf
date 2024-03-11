e7681dc75eb8fce57ae0999cc4f2ca3a329c8c51
Alberto Castañeda <alberto.castaneda@wizeline.com>
2024-03-11 15:47:01 -0600

    fix: revert #307 (#308)

--- infrastructure/terraform/service-accounts.tf
+++
@@ -1,7 +0,0 @@
-# Service Account for WizePrompt team to Connect to CloudSQL from AWS
-resource "google_service_account" "wizeprompt" {
-  display_name = "WizePrompt - CloudSQL Connect"
-  account_id   = "wizeprompt"
-  description  = "Allow WizePrompt team to connect to CloudSQL with read-only privileges"
-  disabled     = false
-}

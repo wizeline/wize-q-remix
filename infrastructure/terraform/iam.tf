4370f5dfff32943bfd1d7a486891f07fa06a4504
Alberto Castañeda <alberto.castaneda@gmail.com>
2024-03-13 15:17:13 -0600

    update WizePrompt read

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -1,7 +1,6 @@
 # CloudSQL Read Permissions for WizePrompt SA
 resource "google_project_iam_member" "wizeprompt__cloudsql__reader" {
+  project = var.project
   role = "roles/cloudsql.viewer"
-  members = [
-    "serviceAccount:${google_service_account.wizeprompt.email}",
-  ]
+  member = "serviceAccount:${google_service_account.wizeprompt.email}"
 }

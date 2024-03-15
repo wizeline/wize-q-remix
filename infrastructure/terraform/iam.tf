078e680a48a4346ba964f573add02c65553949d8
Alberto Castañeda <alberto.castaneda@gmail.com>
2024-03-15 14:26:22 -0600

    fix iam TF project name casing

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -1,6 +1,6 @@
 # CloudSQL Read Permissions for WizePrompt SA
 resource "google_project_iam_member" "wizeprompt__cloudsql__reader" {
-  project = var.project
+  project = "wizeline-questions"
   role = "roles/cloudsql.viewer"
   member = "serviceAccount:${google_service_account.wizeprompt.email}"
 }
